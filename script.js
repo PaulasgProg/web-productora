6b5719ad4c06f42f0a1f2354466aed2e2900d0c3
Paula <paula.sotelo.99@gmail.com>
2025-03-26 18:08:13 +0100

    vista multiplataforma

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function mostrarSeccion(id) {
 
     // Mover el menú a la izquierda
     menu.classList.add("move-left");
+    menu.style.position="fixed"
 
     // Mostrar el contenido
     content.classList.add("show");
@@ -12,6 +13,18 @@ function mostrarSeccion(id) {
     document.querySelectorAll(".seccion").forEach(seccion => {
         seccion.classList.remove("visible");
     });
+    if (window.innerWidth <= 768) {
+        menu.style.display="none"
+        menu.style.width="100%"
+        menu.style.transform="translateX(-100%)"
+        menu.classList.remove("show")
+    }else{
+        menu.style.display="block"
+        menu.style.width="auto"
+        menu.style.transform="translateX(0)"
+        menu.classList.add("show")
+    }
+    
 
     // Mostrar la sección seleccionada
     document.getElementById(id).classList.add("visible");
@@ -22,14 +35,44 @@ document.getElementById("logo").addEventListener("click", function() {
     const menu = document.getElementById("menu");
     const content = document.getElementById("content");
 
-    // Regresar el menú al centro
-    menu.classList.remove("move-left");
+    menu.style.position="block"
+    if (window.innerWidth <= 768) {
+        menu.style.width="100%"
+        /*menu.style.transform="translateX(0)"*/
+    }else{
+        // Regresar el menú al centro
+        menu.style.transform="translateX(-50%)"
+        menu.style.width="auto"
+        menu.classList.remove("move-left");
+
+    }
 
     // Ocultar el contenido
     content.classList.remove("show");
 
+
     // Ocultar todas las secciones
     document.querySelectorAll(".seccion").forEach(seccion => {
         seccion.classList.remove("visible");
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+
+    // Asegurar que el menú comience oculto en pantallas pequeñas
+    if (window.innerWidth <= 768) {
+       // menu.classList.remove("show");
+    }
+
+    menuToggle.addEventListener("click", function () {
+        menu.classList.toggle("show");
+        menu.style.display="block"
+        menu.style.width="100%"
+        menu.style.transform="translateX(0)"
+        menu.classList.add("show")
+        
     });
 });
